44211e3843f77de56a82bcf48062240fcfb425f7
gennigasilya <gennigasilya@gmail.com>
2026-08-12 20:43:07 +0700

    WR
    Updated dotenv configuration to specify the path for the .env file.

--- _github_upload/server/index.js
+++ _github_upload/server/index.js
@@ -1,6 +1,6 @@
-require("dotenv").config();
-const express = require("express");
 const path = require("path");
+require("dotenv").config({ path: path.join(__dirname, ".env") });
+const express = require("express");
 const cookieParser = require("cookie-parser");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
